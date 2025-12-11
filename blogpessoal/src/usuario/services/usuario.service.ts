1fd0d03ccce59fa8b38b548bd3d5e0127041cf92
eduardo.reis <eduardo.olvreis@gmail.com>
2025-12-11 15:19:06 -0300

    feat: incluido o auth no projeto

--- blogpessoal/src/usuario/services/usuario.service.ts
+++ blogpessoal/src/usuario/services/usuario.service.ts
@@ -20,6 +20,24 @@ export class UsuarioService {
         })
     }
 
+    async findAll(): Promise<Usuario[]>{
+        return await this.usuarioRepository.find()
+    }
+
+    async findById(id: number): Promise<Usuario>{
+        const usuario = await this.usuarioRepository.findOne({
+            where:{
+                id
+            }
+        })
+
+        if(!usuario){
+            throw new HttpException("Usuário não encontrado!", HttpStatus.NOT_FOUND)
+        }
+
+        return usuario
+    }
+
     async create(usuario: Usuario): Promise<Usuario>{
         const buscaUsuario = await this.findByUsuario(usuario.usuario)
         
@@ -29,5 +47,17 @@ export class UsuarioService {
         
         usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha)
         return await this.usuarioRepository.save(usuario)
+    }
+
+    async update(usuario: Usuario): Promise<Usuario>{
+        await this.findById(usuario.id);
+
+        const buscaUsuario = await this.findByUsuario(usuario.usuario);
+
+        if (buscaUsuario && buscaUsuario.id !== usuario.id)
+            throw new HttpException('Usuário (e-mail) já Cadastrado!', HttpStatus.BAD_REQUEST);
+
+        usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha)
+        return await this.usuarioRepository.save(usuario);
     }
 }
